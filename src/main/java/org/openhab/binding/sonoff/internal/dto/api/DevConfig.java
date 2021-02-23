c2d1243092067f98def42acd4da00bbda05366a3
Delid4ve <56836577+delid4ve@users.noreply.github.com>
2021-02-23 14:31:52 +0000

    Delete src/main directory

--- src/main/java/org/openhab/binding/sonoff/internal/dto/api/DevConfig.java
+++
@@ -1,9 +0,0 @@
-
-package org.openhab.binding.sonoff.internal.dto.api;
-
-import java.io.Serializable;
-
-public class DevConfig implements Serializable {
-
-    private final static long serialVersionUID = -3528893989005176175L;
-}
